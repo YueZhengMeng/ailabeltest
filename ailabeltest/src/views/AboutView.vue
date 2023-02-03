1618417cf6055553cb8e698016744f5452353b86
乐正萌 <804570270@qq.com>
2023-02-03 17:33:37 +0800

    Initial commit

---
+++ ailabeltest/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ailabel/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ailabel from '@/components/ailabel.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    ailabel
+  }
+}
+</script>
